d0577b1255bb56adcb2873ea079f0a6695de04fb
Andreas Plaß <andreas.plass@haw-hamburg.de>
2016-10-11 15:42:09 +0200

    Release

--- OscillatorOnOff/envelope.cpp
+++ OscillatorOnOff/envelope.cpp
@@ -2,21 +2,60 @@
 #include <math.h>
 #include "envelope.h"
 
+const float MIN_GAIN_DB = -180;
+const float MIN_GAIN = pow(10, MIN_GAIN_DB/20);
+
+inline float db2gain(float db){
+    return pow(10, db/20);
+}
+inline float gain2db(float gain){
+    return 20*log(gain);
+}
+
 Envelope::Envelope()
-    : gain(0)
-{}
+    : gain(0), sampleRate(0), state(OFF), releaseSeconds(0)
+{
+    // zu Testzwecken 1 Sekunde Releasezeit
+    setReleaseSeconds(5);
+}
 void Envelope::setSampleRate(float sampleRate){
     this->sampleRate = sampleRate;
 }
+void Envelope::setReleaseSeconds(float seconds){
+    this->releaseSeconds = seconds;
+}
 
 void Envelope::on(){
-    gain = 1;
+    setState(ON);
 }
 void Envelope::off(){
-    gain = 0;
+    setState(RELEASE);
 }
 
+void Envelope::setState(State state){
+    this->state = state;
+    if (state == OFF){
+        gain = 0;
+        qDebug() << "OFF";
+    }
+    if (state == ON){
+        gain = gainFactor = 1;
+        qDebug() << "ON";
+    }
+    if (state == RELEASE){
+        float gainFactor_dB
+                = fabs(0 - MIN_GAIN_DB)/(sampleRate * releaseSeconds);
+        gainFactor = 1/db2gain(gainFactor_dB);
+    }
+
+}
 
 float Envelope::process(float input){
-    return gain * input;
+    gain *= gainFactor;
+    float output = gain * input;
+
+    if (gain < MIN_GAIN && state == RELEASE){
+        setState(OFF);
+    }
+    return output;
 }
--- OscillatorOnOff/envelope.h
+++ OscillatorOnOff/envelope.h
@@ -5,14 +5,21 @@
 class Envelope
 {
 public:
+    enum State{OFF, ON, RELEASE};
     Envelope();
     void setSampleRate(float sampleRate);
     float process(float input);
     void on();
     void off();
+    void setReleaseSeconds(float seconds);
+private:
+    void setState(State state);
 private:
     float sampleRate;
     float gain;
+    float releaseSeconds;
+    State state;
+    float gainFactor;
 };
 
 #endif // ENVELOPE_H
